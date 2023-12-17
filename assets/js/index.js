f402e099a4d75370687bd2cd11fdee66a83f64a3
Ctk334 <104793014+Ctk334@users.noreply.github.com>
2023-12-17 11:40:04 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,19 @@
+function sendMail(){
+    var params ={
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+        };
+    const  serviceID = "service_2jcr0h7";
+    const  templateID="template_lzvek6m"; 
+
+    emailjs.send(serviceID, templateID, params).then((res) => { 
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("your message sent successfully");
+    })
+    .catch((err) => console.log(err));
+}
 
