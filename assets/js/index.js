3db69c4bda7c89e3c4bb2ba72c5a2dfc1fd28197
Ctk334 <104793014+Ctk334@users.noreply.github.com>
2023-12-20 08:38:51 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,13 @@ function sendMail(){
     var params ={
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
-        message: document.getElementById("message").value
+        message: document.getElementById("message").value,
         };
     const  serviceID = "service_2jcr0h7";
-    const  templateID="template_lzvek6m"; 
+    const  templateID = "template_lzvek6m";
 
-    emailjs.send(serviceID, templateID, params).then((res) => { 
+    emailjs.send(serviceID,templateID,params)
+      .then((res) => { 
         document.getElementById("name").value = "";
         document.getElementById("email").value = "";
         document.getElementById("message").value = "";
